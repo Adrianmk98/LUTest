96cbfa40edc1f5551834fd753dd14ea42f519480
Adrian <adrianmkrzyzanowski@gmail.com>
2025-02-28 16:48:09 -0500

    -I didnt want to leave it unfinished. Please do not view this version as it was completed after the allotted time

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import json
+from jinja2 import Template
+
 #I know I shouldnt make the API key public but its the demo
+#I didnt want to leave the code unfinished. Please look at the older version
 API_Key="l7xx2af7939c63424511946e0fcdc35fe22a"
 Base_URL="https://api-eu.hosted.exlibrisgroup.com/almaws/v1"
 Auth={
@@ -18,7 +21,7 @@ def get_users():
 
 
 #gets all load information
-def get_Loan(userid):
+def get_loan(userid):
     loan_URL=Base_URL+"/users/"+userid+"/loans"
     response=requests.get(loan_URL,headers=Auth,params={"limit":50})
     if response.status_code==200:
@@ -27,7 +30,7 @@ def get_Loan(userid):
 
 #gets all load information
 def get_fines(userid):
-    fines_URL=Base_URL+"/users/"+userid+"/fines"
+    fines_URL=Base_URL+"/users/"+userid+"/fees"
     response=requests.get(fines_URL,headers=Auth,params={"limit":50})
     if response.status_code==200:
         fines= response.json().get("fee",[])
@@ -44,32 +47,48 @@ def generate_html():
         userid=(user.get("primary_id"))
         f_name=user.get("first_name","X")
         l_name=user.get("last_name","X")
-        loans=get_Loan(userid)
-        fines=get_fines(userid)
+        loans=get_loan(userid)
+        fines,total_fines=get_fines(userid)
         user_data.append(
             {
                 "userid":userid,
                 "name":f_name+" "+l_name,
                 "loans":loans,
-                "fines":fines
+                "fines": fines,
+                "total_fines": total_fines
             }
         )
-        print(f"Name: {f_name}")
-        print(f"User ID: {userid}")
-        print(f"Loans: {json.dumps(loans, indent=4)}")
-        print(f"Fines: {json.dumps(fines, indent=4)}")
-        '''
-    htmlbase=<html><head></head><body>
+
+        #print(f"Name: {f_name}")
+        #print(f"User ID: {userid}")
+        #print(f"Loans: {json.dumps(loans, indent=4)}")
+        #print(f"Fines: {json.dumps(fines, indent=4)}")
+    user_data.sort(key=lambda x: x["total_fines"], reverse=True)
+    htmlbase=Template('''<html><body>
     <table>
-    <tr>
-    <th>ID</th>
-    <th>Name</th>
-    <th>Loans</th>
-    <th>Fees</th>
-    </tr>
-    <tr>
-    <td></td>
+            <tr>
+                <th>Name</th>
+                <th>User ID</th>
+                <th>Number of Loans</th>
+                <th>Total Fines ($)</th>
+            </tr>
+            {% for user in users %}
+            <tr>
+                <td>{{ user.name }}</td>
+                <td>{{ user.userid }}</td>
+                <td>{{ user.loans | length }}</td>
+                <td>{{ "%.2f" | format(user.total_fines) }}</td>
+            </tr>
+            {% endfor %}
+        </table>
     </body></html>
-'''
+''')
+    html_content = htmlbase.render(users=user_data)
+
+    # Save to file
+    with open("report.html", "w", encoding="utf-8") as file:
+        file.write(html_content)
+
+    print("Report generated: alma_users_report.html")
 
 generate_html()
